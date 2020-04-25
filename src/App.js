1d70e91fec95258dd0ba75511386c86c7dd54547
Andrea Serafin <andrea.serafin@qmi.it>
2020-04-26 00:20:57 +0200

    pages

--- src/App.js
+++ src/App.js
@@ -2,54 +2,20 @@ import React, { useEffect } from "react";
 import gsap from "gsap";
 import "./styles/App.scss";
 
-import Header from "./components/Header";
-import Banner from "./components/Banner";
-import Cases from "./components/Cases";
-import IntroOverlay from "./components/IntroOverlay";
+import Home from "./pages/Home";
 
 const App = () => {
   useEffect(() => {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
 
+    //prevent flash body on refresh
     gsap.to("body", 0, { css: { visibility: "visible" } });
-
-    const tl = gsap.timeline();
-
-    tl.from(".line span", 1.8, {
-      y: 100,
-      ease: "power4.out",
-      delay: 0.8,
-      skewY: 7,
-      stagger: {
-        amount: 0.3,
-      },
-    })
-      .to(".overlay-top", 1.6, {
-        height: 0,
-        ease: "expo.inOut",
-        delay: -0.6,
-        // stagger: {
-        //   amount: 0.4,
-        // },
-      })
-      .to(".overlay-bottom", 1.6, {
-        width: 0,
-        ease: "expo.inOut",
-        delay: -0.4,
-        stagger: {
-          amount: 0.2,
-        },
-      })
-      .to(".intro-overlay", 0, { css: { display: "none" } });
   }, []);
 
   return (
     <>
-      <IntroOverlay />
-      <Header />
-      <Banner />
-      <Cases />
+      <Home />
     </>
   );
 };
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import gsap from "gsap";
+
+import IntroOverlay from "../../components/IntroOverlay";
+import Header from "../../components/Header";
+import Banner from "../../components/Banner";
+import Cases from "../../components/Cases";
+
+const Home = () => {
+  useEffect(() => {
+    const tl = gsap.timeline();
+
+    tl.from(".line span", 1.8, {
+      y: 100,
+      ease: "power4.out",
+      delay: 0.8,
+      skewY: 7,
+      stagger: {
+        amount: 0.3,
+      },
+    })
+      .to(".overlay-top", 1.6, {
+        height: 0,
+        ease: "expo.inOut",
+        delay: -0.6,
+        // stagger: {
+        //   amount: 0.4,
+        // },
+      })
+      .to(".overlay-bottom", 1.6, {
+        width: 0,
+        ease: "expo.inOut",
+        delay: -0.4,
+        stagger: {
+          amount: 0.2,
+        },
+      })
+      .to(".intro-overlay", 0, { css: { display: "none" } });
+  }, []);
+  return (
+    <>
+      <IntroOverlay />
+      <Header />
+      <Banner />
+      <Cases />
+    </>
+  );
+};
+
+export default Home;
