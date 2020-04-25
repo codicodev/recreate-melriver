9650f35e3604a6a89015f20359424f867fb462e8
Andrea Serafin <andrea.serafin@qmi.it>
2020-04-25 16:05:42 +0200

    clean code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
