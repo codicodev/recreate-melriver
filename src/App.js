7e403d579f1928441a0351b30d59ccae75eacf3d
Andrea Serafin <andrea.serafin@qmi.it>
2020-04-25 23:23:48 +0200

    added animation intro

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import gsap from "gsap";
 import "./styles/App.scss";
 
 import Header from "./components/Header";
@@ -10,6 +11,36 @@ const App = () => {
   useEffect(() => {
     const vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    gsap.to("body", 0, { css: { visibility: "visible" } });
+
+    const tl = gsap.timeline();
+
+    tl.from(".line span", 1.8, {
+      y: 100,
+      ease: "power4.out",
+      delay: 0.8,
+      skewY: 7,
+      stagger: {
+        amount: 0.3,
+      },
+    })
+      .to(".overlay-top", 1.6, {
+        height: 0,
+        ease: "expo.inOut",
+        delay: -0.6,
+        stagger: {
+          amount: 0.4,
+        },
+      })
+      .to(".overlay-bottom", 1.6, {
+        width: 0,
+        ease: "expo.inOut",
+        delay: -0.4,
+        stagger: {
+          amount: 0.4,
+        },
+      });
   }, []);
 
   return (
