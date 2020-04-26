8e9d9c80355153159578073bf2ed5512b3cc9580
Andrea Serafin <andrea.serafin@qmi.it>
2020-04-26 16:40:48 +0200

    unmount intro on complete animation

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import gsap from "gsap";
 
 import IntroOverlay from "../../components/IntroOverlay";
@@ -6,40 +6,54 @@ import Header from "../../components/Header";
 import Banner from "../../components/Banner";
 import Cases from "../../components/Cases";
 
-const Home = () => {
-  useEffect(() => {
-    const tl = gsap.timeline();
+const homeAnimation = (completeAnimation) => {
+  const tl = gsap.timeline();
 
-    tl.from(".line span", 1.8, {
-      y: 100,
-      ease: "power4.out",
-      delay: 0.8,
-      skewY: 7,
+  tl.from(".line span", 1.8, {
+    y: 100,
+    ease: "power4.out",
+    delay: 0.8,
+    skewY: 7,
+    stagger: {
+      amount: 0.3,
+    },
+  })
+    .to(".overlay-top", 1.6, {
+      height: 0,
+      ease: "expo.inOut",
+      delay: -0.6,
+      // stagger: {
+      //   amount: 0.4,
+      // },
+    })
+    .to(".overlay-bottom", 1.6, {
+      width: 0,
+      ease: "expo.inOut",
+      delay: -0.4,
       stagger: {
-        amount: 0.3,
+        amount: 0.2,
       },
     })
-      .to(".overlay-top", 1.6, {
-        height: 0,
-        ease: "expo.inOut",
-        delay: -0.6,
-        // stagger: {
-        //   amount: 0.4,
-        // },
-      })
-      .to(".overlay-bottom", 1.6, {
-        width: 0,
-        ease: "expo.inOut",
-        delay: -0.4,
-        stagger: {
-          amount: 0.2,
-        },
-      })
-      .to(".intro-overlay", 0, { css: { display: "none" } });
+    .to(".intro-overlay", 0, {
+      css: { display: "none" },
+      onComplete: completeAnimation,
+    });
+};
+
+const Home = () => {
+  const [animationComplete, setAnimationComplete] = useState(false);
+
+  const completeAnimation = () => {
+    console.log("animationComplete");
+    setAnimationComplete(true);
+  };
+
+  useEffect(() => {
+    homeAnimation(completeAnimation);
   }, []);
   return (
     <>
-      <IntroOverlay />
+      {!animationComplete ? <IntroOverlay /> : null}
       <Header />
       <Banner />
       <Cases />
